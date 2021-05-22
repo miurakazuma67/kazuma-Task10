b62d5076dd3ded976aa07d9e5972d86d94444583
miurakazuma67 <68637266+miurakazuma67@users.noreply.github.com>
2021-05-22 18:30:47 +0900

    完成

---
+++ Task10/PrefectureTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PrefectureTableViewCell.swift
+//  Task10
+//
+//  Created by 三浦　一真 on 2021/05/22.
+//
+
+import UIKit
+
+class PrefectureTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var prefectureLabel: UILabel!
+    @IBOutlet weak var orderLabel: UILabel!
+    
+    static var identifier: String { String(describing: self) }
+    static var nib: UINib { UINib(nibName: String(describing: self), bundle: nil) }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
---
+++ Task10/prefectureViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Task10
+//
+//  Created by 三浦　一真 on 2021/05/22.
+//
+
+import UIKit
+
+class PrefectureViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let prefectures: [String] = [
+        "北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県",
+        "茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県",
+        "新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県",
+        "静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県",
+        "奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県",
+        "徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県",
+        "熊本県","大分県","宮崎県","鹿児島県","沖縄県"
+    ]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(PrefectureTableViewCell.nib, forCellReuseIdentifier: PrefectureTableViewCell.identifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return prefectures.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: PrefectureTableViewCell.identifier, for: indexPath) as! PrefectureTableViewCell
+        let index = indexPath.row
+        
+        cell.prefectureLabel?.text = prefectures[index]
+        cell.orderLabel?.text = "\(index + 1)番目の都道府県です"
+        switch index % 3 {
+        case 0:
+            cell.backgroundColor = UIColor.systemRed
+        case 1:
+            cell.backgroundColor = UIColor.systemGreen
+        default:
+            cell.backgroundColor = UIColor.systemBlue
+        }
+        return cell
+    }
+}
+
